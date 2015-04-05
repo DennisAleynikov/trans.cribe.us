408e3fc82e7e4ba361ee0a1ffb8b34842d350df5
Dennis Aleynikov <daleynikov@gmail.com>
2015-04-04 18:02:34 -0700

    JS Linted for stability + white statusbar

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -4,50 +4,53 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('transcribe', ['ionic', 'transcribe.controllers'])
+angular.module('transcribe', ['ionic', 'ngCordova', 'transcribe.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
+  .run(function ($ionicPlatform, $cordovaStatusbar) {
+    $ionicPlatform.ready(function () {
+      // Make the statusbar white
+      $cordovaStatusbar.overlaysWebView(true)
+      $cordovaStatusBar.style(1)
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
   })
-    .state('app.record', {
-      url: "/record",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/record.html",
-          controller: 'RecordCtrl'
-        }
-      }
-    })
 
-  .state('app.single', {
-    url: "/meeting/:meetingId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/meeting.html",
-        controller: 'MeetingCtrl'
-      }
-    }
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+      .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "templates/menu.html",
+        controller: 'AppCtrl'
+      })
+      .state('app.record', {
+        url: "/record",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/record.html",
+            controller: 'RecordCtrl'
+          }
+        }
+      })
+
+      .state('app.single', {
+        url: "/meeting/:meetingId",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/meeting.html",
+            controller: 'MeetingCtrl'
+          }
+        }
+      });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/record');
   });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/record');
-});
--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -1,32 +1,55 @@
 angular.module('transcribe.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
-  $scope.meetings = [
-    {title: 'Yearly Review', time: 'Yesterday', id:"I83N"},
-    {title: 'Daily Lunch', time: 'Tomorrow', id:"B3NE"},
-    {title: 'Idea Brainstorm', time: 'Last night', id:"7hhC"},
-    {title: 'Random Meeting', time: 'Random time', id:"X4f2"}];
+  .controller('AppCtrl', function ($scope, $ionicActionSheet, $timeout) {
+    // Form data for the login modal
+    $scope.loginData = {};
+    $scope.meetings = [
+      {title: 'Yearly Review', time: 'Yesterday', id: "I83N"},
+      {title: 'Daily Lunch', time: 'Tomorrow', id: "B3NE"},
+      {title: 'Idea Brainstorm', time: 'Last night', id: "7hhC"},
+      {title: 'Random Meeting', time: 'Random time', id: "X4f2"}];
 
-  $scope.record = function() {
-    alert("hi");
-  }
-})
+    $scope.record = function () {
+      // Show the action sheet
+      var hideSheet = $ionicActionSheet.show({
+        buttons: [
+          { text: '<b>Share</b> This' },
+          { text: 'Move' }],
+        destructiveText: 'Delete',
+        titleText: 'Modify your album',
+        cancelText: 'Cancel',
+        cancel: function () {
+          console.log('cancel');
+        },
+        buttonClicked: function (index) {
+          console.log(index);
+          return true;
+        }
+      });
 
-.controller('RecordCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+      // For example's sake, hide the sheet after two seconds
+      $timeout(function () {
+        hideSheet();
+      }, 2000);
+    };
+  })
 
-.controller('MeetingCtrl', function($scope, $stateParams) {
+  .controller('RecordCtrl', function ($scope) {
+    var apiKey = 45199562;
+    var sessionId = '2_MX40NTE5OTU2Mn5-MTQyODE5NDA2NTIyMX5ZRElTb2UvaEp6b3JrZUFHbUMwYmxNY29-fg';
+    var session = OT.initSession(apiKey, sessionId);
+    var token = 'T1==cGFydG5lcl9pZD00NTE5OTU2MiZzaWc9MDEyZWZkZmIwNGY4NGE3ODFlY2Q1NDY2MjhhZTZhYTg0ZTAxN2E0Mzpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTJfTVg0ME5URTVPVFUyTW41LU1UUXlPREU1TkRBMk5USXlNWDVaUkVsVGIyVXZhRXA2YjNKclpVRkhiVU13WW14TlkyOS1mZyZjcmVhdGVfdGltZT0xNDI4MTk0MTI4Jm5vbmNlPTAuOTg5OTk3NDMyMDU0MjAyNyZleHBpcmVfdGltZT0xNDI4MjgwNTI4';
+    session.connect(token, function(error) {
+      if (error) {
+        console.log(error.message);
+      } else {
+        console.log('connected to session');
+      }
+    });
+  })
+
+  .controller('MeetingCtrl', function ($scope, $stateParams) {
     $scope.title = $stateParams.meetingId;
     $scope.user = {name: 'dennis', pic: 'http://i.imgur.com/N0HqWH0.jpg'};
-    $scope.transcript = [{m:"Maecenas sed diam eget risus varius blandit sit amet non magna.",u:'dennis'},{m:"Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nulla vitae elit libero, a pharetra augue. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.", u:'ray'},{m:"Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nulla vitae elit libero, a pharetra augue.", u:'anne'}];
-});
+    $scope.transcript = [{m: "Maecenas sed diam eget risus varius blandit sit amet non magna.", u: 'dennis'}, {m: "Maecenas sed diam eget risus varius blandit sit amet non magna. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nulla vitae elit libero, a pharetra augue. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.", u: 'ray'}, {m: "Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nulla vitae elit libero, a pharetra augue.", u: 'anne'}];
+  });
